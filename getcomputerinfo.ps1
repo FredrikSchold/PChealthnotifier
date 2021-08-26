5a7443d3506a7dea00ce31044b2e9619dd7fe04b
Fredrik Schöld <71184994+FredrikSchold@users.noreply.github.com>
2021-08-26 15:17:36 +0200

    Update getcomputerinfo.ps1

--- getcomputerinfo.ps1
+++ getcomputerinfo.ps1
@@ -1,3 +1,6 @@
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
+
 function Show-Notification {
     [cmdletbinding()]
     Param (
@@ -11,8 +14,8 @@ function Show-Notification {
     $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
 
     $RawXml = [xml] $Template.GetXml()
-    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
-    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+    ($RawXml.toast.visual.binding.text|Where-Object {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text|Where-Object {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
 
     $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
     $SerializedXml.LoadXml($RawXml.OuterXml)
@@ -26,19 +29,27 @@ function Show-Notification {
     $Notifier.Show($Toast);
 }
 
-
-
 try {
+    Write-Verbose "Checking disksize..."
     $size = get-WmiObject win32_logicaldisk -filter "DriveType = '3'" | Select-Object -Property freespace,size
     $total_Size = [Math]::Round($size.size /1gb)
     $size_Left = [Math]::Round($size.freespace /1gb)
 
-    if ($size_Left -lt ($total_Size / 10)) {
+    Write-Verbose "Disk Space left = $size_Left GB "
+
+    if ($size_Left -lt ($total_Size / 20)) {
         Show-Notification -ToastTitle "Disk Space" -ToastText "You only have a small amount of diskspace left ($size_Left GB) available on your C: Drive. Your PC might not function correctly until some diskspace is freed"
+        Write-Verbose "Notification text shown 'You only have a small amount of diskspace left ($size_Left GB) available on your C: Drive. Your PC might not function correctly until some diskspace is freed' "
+    }
+
+    else {
+        Write-Verbose "Size left is greater than 5%."
     }
    
 }
 catch {
-
-    Continue
+    Write-Verbose "Could not get Diskinfo.. Exiting"
+    Exit 0
 }
+
+Exit 0
